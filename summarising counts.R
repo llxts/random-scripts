92f403db913d38431c5df462b3294e6a8b5601ee
llxts <68283863+llxts@users.noreply.github.com>
2024-07-20 11:07:01 +0800

    Update summarising counts.R

--- summarising counts.R
+++ summarising counts.R
@@ -3,7 +3,7 @@ library(stringr)
 
 #get list of files in folder
 
-filelist <- list.files(path = "./CountData", recursive = TRUE,
+filelist <- list.files(path = "./Counts", recursive = TRUE,
                        pattern = "\\.txt$", 
                        full.names = TRUE)
 
@@ -18,34 +18,37 @@ df <- filelist %>%
   map_df(read_table, col_names = FALSE, .id = "FileName")
 
 
-df %>% separate(X1, c('CRE', 'BC'), sep = "_") -> df
+df %>% separate(X1, c('CRE', 'BC'), sep = "_") %>% drop_na() -> df
+
 
 colnames(df)[4] <- 'BC_count' #change colname to bc count
 
 #formating for mpranalyze
 df_mpra <- df
-df_mpra$FileName <- gsub(pattern = "./CountData/", replacement = "", x = df_mpra$FileName, fixed = TRUE)
-df_mpra$FileName <- gsub(pattern = "counts_bowtie_F268.txt", replacement = "", x = df_mpra$FileName, fixed = TRUE)
+df_mpra$FileName <- gsub(pattern = "./Counts/", replacement = "", x = df_mpra$FileName, fixed = TRUE)
+df_mpra$FileName <- gsub(pattern = "counts.txt", replacement = "", x = df_mpra$FileName, fixed = TRUE)
 df_mpra$SamType <- lapply(X = df_mpra$FileName, FUN = function(t) if (grepl("DNA", t)) "DNA" else "RNA")
 df_mpra$Organ <- lapply(X = df_mpra$FileName, FUN = function(t) if (grepl("H", t)) "Heart" else "Liver")
 df_mpra$SampleNo <- str_extract(df_mpra$FileName, "\\d+")
 
 #pivot table, grouping multiple variables
-mpra_h <- df_mpra[-1] %>% group_by(Organ) %>%filter(grepl("Heart", Organ)) %>% pivot_wider(names_from = c(SamType, SampleNo, BC), values_from = BC_count, names_sep = ".", values_fill = 0)
-mpra_h <- mpra_h[-2]
-mpra_l <- df_mpra[-1] %>% group_by(Organ) %>%filter(grepl("Liver", Organ)) %>% pivot_wider(names_from = c(SamType, SampleNo, BC), values_from = BC_count, names_sep = ".", values_fill = 0)
-mpra_l <- mpra_h[-2]
+mpra_dna <- df_mpra[-1] %>% group_by(SamType) %>%filter(grepl("DNA", SamType)) %>% pivot_wider(names_from = c(SamType, Organ, SampleNo, BC), values_from = BC_count, names_sep = ".", values_fill = 0)
+mpra_dna <- mpra_dna[-2]
+mpra_rna <- df_mpra[-1] %>% group_by(SamType) %>%filter(grepl("RNA", SamType)) %>% pivot_wider(names_from = c(SamType, Organ, SampleNo, BC), values_from = BC_count, names_sep = ".", values_fill = 0)
+mpra_rna <- mpra_rna[-2]
 
+write.csv(mpra_dna, "mpra-dna.csv", col.names = TRUE)
+write.csv(mpra_rna, "mpra-rna.csv", col.names = TRUE)
 
 #group data by filename and cre to come up with the number of retrieved barcodes per cre per sample
-df_BCperSam <- df %>% group_by(FileName, CRE)%>%summarize(retrievedBC = n(), .groups='drop', sumBC = sum(BC_count, na.rm = TRUE))
+df %>% group_by(FileName, CRE)%>%summarize(retrievedBC = n(), .groups='drop', sumBC = sum(BC_count, na.rm = TRUE)) -> df_BCperSam
 
 #now we are ready to do the calculations. as per MPRAflow, normalising the insert = (no. of bc +1)/(total number of bc retrieved + 1)/total counts * 10**6
 #add = true to augment the grouping since group_by overrides the existing grouping variables
 
 df_normCounts <- df_BCperSam %>% group_by(FileName)%>%mutate(totalCount = sum(sumBC)) %>% group_by(CRE, .add = TRUE) %>% summarise(normInsertCount = (sumBC + 1)/(retrievedBC+1)/(totalCount) * 10**6)
-df_normCounts$SampleName <-  gsub(pattern = "./CountData/", replacement = "", x = df_normCounts$FileName, fixed = TRUE)
-df_normCounts$SampleName <-  gsub(pattern = "counts_bowtie_F268.txt", replacement = "", x = df_normCounts$SampleName, fixed = TRUE)
+df_normCounts$SampleName <-  gsub(pattern = "./Counts/", replacement = "", x = df_normCounts$FileName, fixed = TRUE)
+df_normCounts$SampleName <-  gsub(pattern = "counts.txt", replacement = "", x = df_normCounts$SampleName, fixed = TRUE)
 df_normCounts$SamType <- lapply(X = df_normCounts$SampleName, FUN = function(t) if (grepl("DNA", t)) "DNA" else "RNA")
 df_normCounts$Organ <- lapply(X = df_normCounts$SampleName, FUN = function(t) if (grepl("H", t)) "Heart" else "Liver")
 df_normCounts$SampleNo <- str_extract(df_normCounts$SampleName, "\\d+")
@@ -81,10 +84,6 @@ df_liver_comb<- df_liver_comb[,-5]
 df_heart_Final <- df_heart_comb  %>% mutate(log2 = log2(`RNA normCount`/`DNA normCount`))
 df_liver_Final <- df_liver_comb  %>% mutate(log2 = log2(`RNA normCount`/`DNA normCount`))
 
-#extract to excel for further analysis
-write.csv(df_heart_Final, "df_heart_Final.csv")
-write.csv(df_liver_Final, "df_liver_Final.csv")
-
 
 #correlation plots
 library(corrplot)
@@ -94,53 +93,155 @@ corrHeart <- df_heart_Final %>% pivot_longer(cols = c("DNA normCount", "RNA norm
 corrHeart$working <- paste(corrHeart$type, "_", corrHeart$SampleNo)
 corrHeart <- corrHeart[-2:-3] %>% pivot_wider(names_from = working, values_from = normCount, values_fill = 0) %>% select(sort(names(.)))
 
-heart_DNA_normcounts <- as.data.frame(corrHeart[2:12], row.names = corrHeart$CRE)
-heart_DNA_normcounts <- log2(heart_DNA_normcounts[,-1])
+corrLiver <- df_liver_Final %>% pivot_longer(cols = c("DNA normCount", "RNA normCount", "log2"), names_to = "type", values_to= "normCount")
+corrLiver$working <- paste(corrLiver$type, "_", corrLiver$SampleNo)
+corrLiver <- corrLiver[-2:-3] %>% pivot_wider(names_from = working, values_from = normCount, values_fill = 0) %>% select(sort(names(.)))
+
+#Save files
+write.csv(corrHeart, "df_heart_Final.csv")
+write.csv(corrLiver, "df_liver_Final.csv")
+
+heart_DNA_normcounts <- as.data.frame(corrHeart[2:12])
+rownames(heart_DNA_normcounts) <- corrHeart$CRE
+heart_DNA_normcounts <- log2(heart_DNA_normcounts)
 heart_DNA_normcounts[heart_DNA_normcounts == "-Inf"]<- 0
 corr_hd <- cor(heart_DNA_normcounts)
 
-heart_RNA_normcounts <- read.delim("heart_RNA_normcounts.txt")
-rownames(heart_RNA_normcounts) <- heart_RNA_normcounts$Row.Labels
-heart_RNA_normcounts <- log2(heart_RNA_normcounts[,-1])
+heart_log2_normcounts <- as.data.frame(corrHeart[13:23])
+rownames(heart_log2_normcounts) <-  corrHeart$CRE
+heart_log2_normcounts[is.na(heart_log2_normcounts)]<- 0
+corr_hlog2 <- cor(heart_log2_normcounts)
+
+heart_RNA_normcounts <- as.data.frame(corrHeart[24:34])
+rownames(heart_RNA_normcounts) <- corrHeart$CRE
+heart_RNA_normcounts <- log2(heart_RNA_normcounts)
 heart_RNA_normcounts[heart_RNA_normcounts == "-Inf"]<- 0
+heart_RNA_normcounts[is.na(heart_RNA_normcounts)]<- 0
 corr_hr <- cor(heart_RNA_normcounts)
 
-liver_DNA_normcounts <- read.delim("liver_DNA_normcounts.txt")
-rownames(liver_DNA_normcounts) <- liver_DNA_normcounts$Row.Labels
-liver_DNA_normcounts <- log2(liver_DNA_normcounts[,-1])
+liver_DNA_normcounts <- as.data.frame(corrLiver[2:12])
+rownames(liver_DNA_normcounts) <- corrLiver$CRE
+liver_DNA_normcounts <- log2(liver_DNA_normcounts)
 liver_DNA_normcounts[liver_DNA_normcounts == "-Inf"]<- 0
 corr_ld <- cor(liver_DNA_normcounts)
 
-liver_RNA_normcounts <- read.delim("liver_RNA_normcounts.txt")
-rownames(liver_RNA_normcounts) <- liver_RNA_normcounts$Row.Labels
-liver_RNA_normcounts <- log2(liver_RNA_normcounts[,-1])
+liver_log2_normcounts <- as.data.frame(corrLiver[13:23])
+rownames(liver_log2_normcounts) <- corrLiver$CRE
+liver_log2_normcounts[is.na(liver_log2_normcounts)]<- 0
+corr_llog2 <- cor(liver_log2_normcounts)
+
+liver_RNA_normcounts <- as.data.frame(corrLiver[24:34])
+rownames(liver_RNA_normcounts) <- corrLiver$CRE
+liver_RNA_normcounts <- log2(liver_RNA_normcounts)
 liver_RNA_normcounts[liver_RNA_normcounts == "-Inf"]<- 0
+liver_RNA_normcounts[is.na(liver_RNA_normcounts)]<- 0
 corr_lr <- cor(liver_RNA_normcounts)
 
-heart_log2 <- read.delim("log2-heart.txt")
-rownames(heart_log2) <- heart_log2$Row.Labels
-heart_log2 <- heart_log2[,-1]
-corr_logH <- cor(heart_log2)
+corrplot.mixed(corr_hlog2, order = 'hclust') 
 
-liver_log2 <- read.delim("log2-liver.txt")
-rownames(liver_log2) <- liver_log2$Row.Labels
-liver_log2 <- liver_log2[,-1]
-corr_logL <- cor(liver_log2)
 
-corrplot.mixed(corr_logL, order = 'hclust')
 
-# general function test 
-#import list of BCs 
-dftest <- read.table("bclist.txt", quote="\"", comment.char="")
-for (i in filelist) {
-  dftest <- read.table(i) %>% full_join(dftest, i, by = c('V1'))
-}
 
-#rename columns
-colnamelist <- as.list(filelist)
-colnamelist <- lapply(X = colnamelist, FUN = function(t) gsub(pattern = "./CountData/", replacement = "", x = t, fixed = TRUE))
-colnamelist <- lapply(X = colnamelist, FUN = function(t) gsub(pattern = "counts_bowtie_F268.txt", replacement = "", x = t, fixed = TRUE))
-colnames(dftest)[2:ncol(dftest)] <- colnamelist
-colnames(dftest)[1] <- "CRE"
+##### MPRAnalyze ######
+
+library(MPRAnalyze)
+ce.dnaCounts <- as.matrix(read.delim("dnacountsrm8.txt", row.names = 1, stringsAsFactors = TRUE))
+ce.rnaCounts <- as.matrix(read.delim("rnacounts_indnarm8.txt", row.names = 1, stringsAsFactors = TRUE))
+ce.dnaAnnot <- as.data.frame(read.delim("dna-annot-rm8.txt", row.names = 1, stringsAsFactors = TRUE))
+ce.rnaAnnot <- as.data.frame(read.delim("rna-annot-rm8.txt", row.names = 1, stringsAsFactors = TRUE))
+ce.dnaAnnot$replicate <- factor(ce.dnaAnnot$Rep)
+ce.dnaAnnot$barcode <- factor(ce.dnaAnnot$BC)
+# ce.dnaAnnot$seqbatch <- factor(ce.dnaAnnot$seqbatch)
+
+ce.rnaAnnot$replicate <- factor(ce.rnaAnnot$Rep)
+ce.rnaAnnot$barcode <- factor(ce.rnaAnnot$BC)
+# ce.rnaAnnot$seqbatch <- factor(ce.rnaAnnot$seqbatch)
+
+
+#reverse the levels
+ce.dnaAnnot$Organ<-factor(ce.dnaAnnot$Organ, levels=rev(levels(ce.dnaAnnot$Organ)))
+ce.rnaAnnot$Organ<-factor(ce.rnaAnnot$Organ, levels=rev(levels(ce.rnaAnnot$Organ)))
+
+
+
+obj <- MpraObject(dnaCounts = ce.dnaCounts, rnaCounts = ce.rnaCounts, rnaAnnot = ce.rnaAnnot, dnaAnnot = ce.dnaAnnot)
+
+## If the library factors are different for the DNA and RNA data, separate 
+## estimation of these factors is needed. We can also change the estimation 
+## method (Upper quartile by default)
+obj <- estimateDepthFactors(obj, lib.factor = c("Rep"),
+                            which.lib = "dna", 
+                            depth.estimator = "uq")
+obj <- estimateDepthFactors(obj, lib.factor = c("Organ", "Rep"),
+                            which.lib = "rna", 
+                            depth.estimator = "uq")
+
+#Empirical testing, just to see which sequence has a regulatory function
+obj1 <- analyzeQuantification(obj = obj,
+                              dnaDesign = ~ Rep + Organ,
+                              rnaDesign = ~ Organ)
+
+##extract alpha values from the fitted model --> gives you transcription rate
+alpha <- getAlpha(obj1, by.factor = "Organ")
+
+
+##visualize the estimates
+boxplot(alpha)
+
+alpha2 <- alpha
+alpha2[is.na(alpha2)] <- 0
+
+## test
+res.heart <- testEmpirical(obj = obj1,
+                           statistic = alpha2$Heart)
+res.liver <- testEmpirical(obj = obj1,
+                           statistic = alpha2$Liver)
+alpha2$Liver_pval <- res.liver$pval.mad
+alpha2$Heart_pval <- res.heart$pval.mad
+
+alpha2$Liver_pvaladj <- p.adjust(alpha2$Liver_pval,method = "fdr")
+alpha2$Heart_pvaladj <- p.adjust(alpha2$Heart_pval,method = "fdr")
+
+par(mfrow=c(2,2))
+
+hist(res.heart$pval.mad, main="heart, all")
+hist(res.liver$pval.mad, main="liver, all")
+
+
+
+#comparing between Organ. the first factor level is the reference -- i.e. heart is the reference. so the more negative logFC, the more heart specific
+obj2 <- analyzeComparative(obj = obj, 
+                           dnaDesign = ~ BC + Rep + Organ, 
+                           rnaDesign = ~ Rep + Organ , 
+                           reducedDesign = ~ Rep)
+
+# comparing using std errors
+objse <- analyzeComparative(obj = obj, 
+                            dnaDesign = ~ BC + Rep + Organ, 
+                            rnaDesign = ~ Organ, 
+                            fit.se=TRUE)
+
+# this tests for the significance of the Organ (i.e. liver or heart) in the model, with Liver expression being used as the contrast and heart as ref i.e. Liver vs Heart
+results.se <- testCoefficient(objse, "Organ", "Heart") # positive logFC indicates that the CRE is more expressed in the heart compared to the liver (logFC is UPREGULATION in Liver vs HEART)
+
+#testing for differential activity
+res <- testLrt(obj2)
+summary (res)
+res2<- res
+res2[is.na(res2)] <- 0
+res2$zscore <- (res2$logFC-mean(res2$logFC))/sd(res2$logFC)
+
+
+
+## plot log Fold-Change
+reslogfc = plot(density(res2$logFC))
+
+## plot volcano (-log10 pval --> higher is better)
+resvolcano = plot(res2$logFC, -log10(res2$pval))
+
+
+##export result
+write.csv(res2, "mpranalyzeresults_noseqbatch_indna_less10-rm8.csv", row.names=TRUE)
+write.csv(alpha2, "mpranalyze_quant-alpha_indna_less10-rm8.csv", row.names=TRUE)
 
 
